82d9e65b220012ff4e4134ee477d9c271d28272b
groszek20 <pea@op.pl>
2019-02-08 23:15:59 +0100

    back to past

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -7,10 +7,9 @@
                 Blank Page
                 <small>Subheading</small>
                 <?php
-                $users = User::find_all_users();
-                foreach ($users as $user){
-                    echo $user->user_id."imie <br>";
-                }
+                $found_user = User::find_user_by_id(1);
+                $user = User::initialize($found_user);
+                echo $user->username;
                 ?>
             </h1>
             <ol class="breadcrumb">
--- includes/user.php
+++ includes/user.php
@@ -19,36 +19,19 @@ class User {
     private static function find_this_query($sql) {
         global $database;
         $result_set = $database->queryPDO($sql);
-        $object_array = array();
-        while ($row = $result_set->fetch()){
-            $object_array[] = self::initialize($row);
-        }
-        return $object_array;
+        return $result_set;
     }
-
-    public static function initialize($records) {
+    public static function initialize($found_user) {
         $the_object = new self;
  
-//        $the_object->username = $found_user["username"];
-//        $the_object->user_id = $found_user["id"];
-//        $the_object->password = $found_user["password"];
-//        $the_object->first_name = $found_user["firstname"];
-//        $the_object->last_name = $found_user["lastname"];
+        $the_object->username = $found_user["username"];
+        $the_object->user_id = $found_user["id"];
+        $the_object->password = $found_user["password"];
+        $the_object->first_name = $found_user["firstname"];
+        $the_object->last_name = $found_user["lastname"];
         
-        foreach ($records as $attribute => $value) {
-            if($the_object->has_the_attribute($attribute)){
-                $the_object->$attribute = $value;
-            }
-        }
         
         return $the_object;
     }
-    
-    private function has_the_attribute($attribute){
-        $object_properties = get_object_vars($this);
-        return array_key_exists($attribute, $object_properties);
-    }
-
 }
-
 ?>
